d4685e187268e081ec75c1b6c08082aa43d32d5b
Martin Dahl-Larsen <martin@mpsoft.dk>
2019-03-18 13:07:58 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+###################################################################
+# auto deploy rainloop webmail (docker)
+###################################################################
+export DEBIAN_FRONTEND=noninteractive
+
+
+cd /usr/src/rainloop-webmail
+
+git pull
+
+rm -rf /var/rainloop-webmail
+
+cp -r /usr/src/rainloop-webmail /var/.
+
+cd /var/rainloop-webmail
+
+docker-compose down --rmi all
+
+service docker restart 
+
+docker-compose build
+
+docker-compose up -d 
